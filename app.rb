4e4bd2e84621dd978a97bd1dbc93319cc2e7357f
Theo <theo.hermansson@itggot.se>
2019-05-24 16:11:19 +0200

    New Repo

---
+++ app.rb
@@ -0,0 +1,252 @@
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'byebug'
+require 'bcrypt'
+require_relative 'model'
+
+enable :sessions
+
+include MyModule
+
+helpers do
+    def get_error()
+        message = session[:msg_login_failed].dup
+        session[:msg_login_failed] = nil
+        return message
+    end
+    def get_validate_error()
+        message = session[:validate_login_error_message].dup
+        session[:validate_login_error_message] = nil
+        return message
+    end
+    def username_validate()
+        message = session[:error_no_info_message].dup
+        session[:error_no_info_message] = nil
+        return message
+    end
+end
+
+
+# Display Landing Page
+#
+get('/') do
+    if session[:username] != nil
+        redirect('/produkter')
+    end
+    slim(:home)
+end
+
+# Display Login Page
+#
+get('/login') do
+    if session[:username] != nil
+        redirect('/produkter')
+    end
+    slim(:login)
+end
+
+# Display Admin Page
+#
+get('/admin') do
+    if session[:username] == nil
+        redirect('/login')
+    end
+    admin(params, session['user_id'])
+    if user_type['User_type'] == "Admin" 
+        slim(:admin)
+    else
+        redirect('/produkter')
+    end
+
+end
+
+
+# Dissplay Admin Ticket Page
+#
+get('/admin/help') do
+    if session[:username] == nil
+        redirect('/login')
+    end
+    admin_tickets(params, session)
+    slim(:admin_help)
+    redirect('/admin/help')
+end
+
+# Removes Tickets
+#
+post('/admin/help') do
+    remove_ticket(params)
+end
+
+# Displays Orders
+#
+get('/admin/beställt') do
+    if session[:username] == nil
+        redirect('/login')
+    end
+    bought_result = bought(params)
+    slim(:beställt, locals:{
+        result: bought_result
+    })
+end
+
+# Performs the login 
+#
+post('/login') do
+    result = login(params, session)
+
+    if result[:validate_login_error]
+        session[:validate_login_error_message] = result[:validate_login_error_message]
+        redirect back 
+    elsif result[:login_error]
+        session[:msg_login_failed] = result[:login_message]
+        redirect back
+    elsif result[:error_no_info]
+        session[:error_no_info_message] = result[:error_no_info_msg]
+        redirect back
+    else
+        session[:user_id] = result[:user_id]
+        session[:username] = result[:username]
+        redirect("/produkter")
+    end
+end
+
+# Display Register
+#
+get('/register') do
+    if session[:username] != nil
+        redirect('/produkter')
+    end
+    slim(:register)
+end
+
+# Performs the login
+#
+post('/register') do
+    register(params)
+end
+
+# Display login failed page
+#
+get('/failed') do
+    slim(:failed)
+end
+
+# Display search page
+#
+get('/search') do
+    slim(:search)
+end
+
+# Performs the search
+#
+post('/search') do
+    search(params)
+    redirect('/search')
+end
+
+# Display profile page
+#
+get('/profil') do
+    if session[:username] == nil
+        redirect('/login')
+    end
+    slim(:profil)
+end
+
+# Performs the logout
+#
+post('/profil') do
+    log_out(params)
+    redirect('/')
+end
+
+# Display the edit profile page
+#
+# @param [Integer] :id, The Id of the user
+#
+get('/edit_profile/:id') do
+    slim(:edit_profile)
+end
+
+# Edits the username
+#
+post('/edit_profile') do
+    edit_profile(params, session)
+end
+
+# Display Moncler Products
+#
+get('/produkter/moncler') do
+    moncler_info = moncler(params, session['user_id'])
+    slim(:moncler, locals:{
+        result: moncler_info
+    })
+end
+
+# Performs the purchase
+#
+post('/produkter/moncler') do
+    buy(params, session)
+end
+
+# Displays main product page
+#
+get('/produkter') do
+    result = produkter(params)
+    slim(:produkter, locals:{
+        result: result
+    })
+end
+
+# Purchase of products
+#
+post('/produkter') do
+    buy(params, session)
+end
+
+# Display Stonge-Island products
+#
+get('/produkter/Stone-Island') do
+    stoneisland_info = stoneisland(params, session['user_id'])
+    slim(:stoneisland, locals:{
+        result: stoneisland_info
+    })
+end
+
+# Performs the purchase of a Stone-Island Product
+#
+post('/produkter/Stone-Island') do
+    buy(params, session)
+end
+
+# Displays the Givenchy products
+#
+get('/produkter/Givenchy') do
+    givenchy_info = givenchy(params, session['user_id'])
+    slim(:givenchy, locals:{
+        result: givenchy_info
+    })
+end
+
+# Performs the purchase of a Givenchy Product
+#
+post('/produkter/Givenchy') do
+    buy(params, session)
+end
+
+# Displays the help page
+#
+get('/help') do
+    if session[:username] == nil
+        redirect('/login')
+    end
+    slim(:kundsupport)
+end
+
+# Send the ticket
+#
+post('/help') do
+    kundsupport(params, session)
+end
---
+++ model.rb
@@ -0,0 +1,259 @@
+module MyModule
+    # Validates the register form
+    # 
+    # param [Hash] Data from params
+    # @option params [String] Username
+    # @option params [String] Password
+    def validate_register(params)
+        if params["Username"].nil? || params["Password"].nil?
+            return false
+        else
+            return true 
+        end
+    end
+
+    # Validates edit profile info
+    #
+    # @param [Hash] Data from params
+    # @option params [String] Username Info
+    def validate_edit_profile(params)
+        if params["Rubrik"].nil?
+            return false
+        else 
+            return true
+        end
+    end
+
+    # Validates login info
+    # 
+    # @param [Hash] Data from params
+    # @option params [String] Username
+    def validate_login(params)
+        if params["Username"].nil? || params["Username"].length > 10 || params["Username"].length < 2 || params["Password"].nil? || params["Password"].length < 2 || params["Password"].length > 10
+            return false
+        else 
+            return true
+        end
+    end
+    
+    # Database
+    def database()
+        db = SQLite3::Database.new("db/db_login.db")
+        db.results_as_hash = true
+        db
+    end
+
+    # Login Function
+    # 
+    # @param [Hash] Data from params
+    # @option params [String] Username
+    # @option params [String] Password
+    # @option params [Integer] Id
+    # 
+    # @return [Hash], Info from Login
+    # 
+    def login(params, session)
+        if validate_login(params)
+            db = database()
+    
+            result = db.execute("SELECT * FROM users WHERE Username = ?", params['Username'])
+            if result.length > 0
+                if BCrypt::Password.new(result[0]["Password"]) == params["Password"]
+                    return {
+                        login_error: false,
+                        user_id: result[0]["Id"],
+                        username: params["Username"]
+                    }
+                else
+                    return {
+                        login_error: true,
+                        login_message: "Incorrect Password or Username"
+                    }
+                end
+            else
+                return {
+                    error_no_info: true,
+                    error_no_info_msg: "Incorrect Password or Username"
+                }
+            end
+        else
+            return {
+                validate_login_error: true,
+                validate_login_error_message: "Password needs to be longer than 2 charcters and less than 10"
+            }
+        end
+    end
+    
+    # Register function
+    # 
+    # @param [Hash] Data from params
+    # @option params [String] Password
+    def register(params)
+        if validate_register(params)
+            db = database()
+            new_password = params["Password"] 
+            hashed_password = BCrypt::Password.create(new_password)
+    
+            db.execute("INSERT INTO users (Username, Password) VALUES (?, ?)", params["Username"], hashed_password)
+        end
+    end
+    
+    # Edit username
+    # 
+    # @param [Hash] Data from params
+    # @option params [String] Password
+    def edit_profile(params, session)
+        if validate_edit_profile(params)
+            db = database()
+    
+            db.execute(%Q(UPDATE users SET Username = '#{params['Rubrik']}' WHERE Id = #{session['user_id']}))
+    
+            session.destroy
+        end
+    end
+
+    # Admin checker
+    #
+    # @param [Hash] Data from params
+    # @param [String] Local Variables
+    # @option params [String] User_Type
+    def admin(params, user_id)
+        db = database()
+    
+        user_type = db.execute('SELECT User_type FROM users WHERE Id = ?', user_id).first
+    end
+    
+    # Log Out function
+    #
+    # @param [Hash] Data from params
+    def log_out(params)
+        session.destroy
+    end
+    
+    # View admin tickets
+    # 
+    # @param [Hash] Data from params
+    # @param [String] Local Variables
+    def admin_tickets(params, session)
+        db = database()
+        session['tickets'] = db.execute('SELECT Id, Username, Ticket FROM support_tickets')
+    end
+    
+    # Ticket remove function
+    #
+    # @param [Hash] Data from params
+    def remove_ticket(params)
+        db = database()
+        db.execute('DELETE FROM support_tickets WHERE Id = ?', params['solve'])
+    end
+    
+    # Moncler product purchase
+    #
+    # @param [Hash] Data from params
+    # @param [String] Local Variables
+    def moncler(params, user_id)
+        db = database()
+        original_price = db.execute('SELECT Pris FROM produkter WHERE Id = 3').first
+        user_type = db.execute('SELECT User_type FROM users WHERE Id = ?', user_id).first 
+        prod_name1 = db.execute('SELECT * FROM produkter WHERE Kategori = "Moncler"')
+        if user_type['User_type'] == "Business"
+            pris = original_price[0] * 0.8
+        else
+            pris = original_price[0]
+        end
+        return {
+            product1: prod_name1,
+            pris: pris
+        }
+    
+    end
+    
+    # Givenchy product purchase
+    #
+    # @param [Hash] Data from params
+    # @param [String] Local Variables
+    def givenchy(params, user_id)
+        db = database()
+        original_price = db.execute('SELECT Pris FROM produkter WHERE Id = 2').first
+        user_type = db.execute('SELECT User_type FROM users WHERE Id = ?', user_id).first
+        prod_name1 = db.execute('SELECT * FROM produkter WHERE Kategori = "Givenchy"')
+        if user_type['User_type'] == "Business"
+            pris = original_price[0] * 0.8
+        else
+            pris = original_price[0]
+        end
+        return {
+            product1: prod_nam1,
+            price: pris
+        }
+    end
+    
+    # Product information
+    #
+    # @param [Hash] Data from params
+    def produkter(params)
+        db = database()
+        prod_name1 = db.execute('SELECT Kategori FROM produkter WHERE Id = "1"')
+        prod_name2 = db.execute('SELECT Kategori FROM produkter WHERE Id = "2"')
+        prod_name3= db.execute('SELECT Kategori FROM produkter WHERE Id = "3"')
+        return {
+            product1: prod_name1,
+            product2: prod_name2,
+            product3: prod_name3
+        }
+    end
+    
+    # Order history
+    #
+    # @param [Hash] Data from params
+    def bought(params)
+        db = database()
+        ordrar = db.execute('SELECT * FROM ordrar INNER JOIN users ON ordrar.Id = users.Id')
+    
+        return {
+            orders: ordrar
+        }
+    end
+    
+    # Stone-Island product purchase
+    #
+    # @param [Hash] Data from params
+    # @param [String] Local Variables
+    def stoneisland(params, user_id)
+        db = database()
+        original_price = db.execute('SELECT Pris FROM produkter WHERE Id = 1').first
+        user_type = db.execute('SELECT User_type FROM users WHERE Id = ?', user_id).first 
+        prod_name1 = db.execute('SELECT * FROM produkter WHERE Kategori = "Stone-Island"')
+        if user_type['User_type'] == "Business"
+            pris = original_price[0] * 0.8
+        else
+            pris = original_price[0]
+        end
+        return {
+            product1: prod_nam1,
+            price: pris
+        }
+    end
+    
+    # Purchase function
+    #
+    # @param [Hash] Data from params
+    # @param [String] Local Variables
+    def buy(params, session)
+        db = database()
+        amount = db.execute('SELECT Amount FROM produkter WHERE Id = ?', params['buy']).first
+        session['product'] = db.execute('SELECT Produkt_Namn FROM produkter WHERE Id = ?', params['buy']).first
+        antal_kvar = amount[0] - 1
+        db.execute('UPDATE produkter SET Amount = ? WHERE Id = ?', antal_kvar, params['buy'])
+        db.execute('INSERT INTO ordrar (Id) VALUES (?)', params['buy'])
+    end
+    
+    # Ticket creater
+    #
+    # @param [Hash] Data from params
+    # @param [String] Local Variables
+    def kundsupport(params, session)
+        db = database()
+        db.execute("INSERT INTO support_tickets (Id, Username, Ticket) VALUES (?, ?, ?)", session['user_id'], session['username'], params["Help"])
+    end
+end
